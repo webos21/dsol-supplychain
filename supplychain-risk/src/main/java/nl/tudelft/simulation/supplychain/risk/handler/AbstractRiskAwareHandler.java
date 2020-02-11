f22e51d794d7c7d373348d800b123dcb2c3def4a
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2020-02-11 21:38:23 +0000

    Share project "supplychain-risk" into "https://svn.tbm.tudelft.nl/SUPPLYCHAIN"

---
+++ supplychain-risk/src/main/java/nl/tudelft/simulation/supplychain/risk/handler/AbstractRiskAwareHandler.java
@@ -0,0 +1,30 @@
+package nl.tudelft.simulation.supplychain.risk.handler;
+
+import org.djutils.event.EventListenerInterface;
+import nl.tudelft.simulation.supplychain.actor.SupplyChainActor;
+import nl.tudelft.simulation.supplychain.handlers.SupplyChainHandler;
+
+/**
+ * AbstractRiskAwareHandler.java. <br>
+ * <br>
+ * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
+ * source code and binary code of this software is proprietary information of Delft University of Technology.
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
+ */
+public abstract class AbstractRiskAwareHandler extends SupplyChainHandler implements EventListenerInterface
+{
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @param owner the owner of this handler
+     */
+    public AbstractRiskAwareHandler(final SupplyChainActor owner)
+    {
+        super(owner);
+    }
+
+
+}
+

11b09dcf31335ab827192b070528a887a188e1b6
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-11-27 10:12:18 +0100

    Added MessagePolicy (originally called Handler)

--- supplychain-actor/src/main/java/nl/tudelft/simulation/actor/message/policy/AbstractMessagePolicy.java
+++ supplychain-actor/src/main/java/nl/tudelft/simulation/actor/message/policy/AbstractMessagePolicy.java
@@ -1,52 +1,67 @@
-package nl.tudelft.simulation.actor.content;
+package nl.tudelft.simulation.actor.message.policy;
 
-import nl.tudelft.simulation.actor.ActorInterface;
-import nl.tudelft.simulation.jstats.streams.StreamInterface;
+import org.djutils.exceptions.Throw;
+
+import nl.tudelft.simulation.actor.Actor;
+import nl.tudelft.simulation.actor.message.MessageType;
 
 /**
- * An abstract definition of a handler, providing a default stream and a constructor method. <br>
+ * An abstract definition of a handler with an owner. <br>
  * <br>
- * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2003-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
  * source code and binary code of this software is proprietary information of Delft University of Technology.
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
  */
-public abstract class AbstractHandler implements HandlerInterface
+public abstract class AbstractMessagePolicy implements MessagePolicyInterface
 {
-    /** the serial version uid */
-    private static final long serialVersionUID = 12L;
+    /** the serial version uid. */
+    private static final long serialVersionUID = 20221126L;
 
-    /** the owner of this handler */
-    private final ActorInterface owner;
-
-    /** the default stream to use for the default time delays */
-    private StreamInterface stream = null;
+    /** the id of the handler. */
+    private final String id;
+    
+    /** the owner of this handler. */
+    private final Actor owner;
+    
+    /** the message type that this handler can handle. */
+    private final MessageType messageType;
 
     /**
-     * constructs a new Handler
-     * @param owner the owner of this handler
+     * constructs a new Handler.
+     * @param id String; the id of the handler
+     * @param owner Actor; the owner of this handler
+     * @param messageType MessageType; the message type that this handler can handle
      */
-    public AbstractHandler(final ActorInterface owner)
+    public AbstractMessagePolicy(final String id, final Actor owner, final MessageType messageType)
     {
-        super();
+        Throw.whenNull(id, "id cannot be null");
+        Throw.whenNull(owner, "owner cannot be null");
+        Throw.whenNull(messageType, "messageType cannot be null");
+        this.id = id;
         this.owner = owner;
-        this.stream = owner.getSimulator().getModel().getStream("default");
+        this.messageType = messageType;
     }
 
-    /**
-     * @return Returns the default stream.
-     */
-    public StreamInterface getStream()
+    /** {@inheritDoc} */
+    @Override
+    public String getId()
     {
-        return this.stream;
+        return this.id;
     }
 
-    /**
-     * @return owner
-     */
-    public ActorInterface getOwner()
+    /** {@inheritDoc} */
+    @Override
+    public Actor getOwner()
     {
         return this.owner;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public MessageType getMessageType()
+    {
+        return this.messageType;
     }
 
 }
--- supplychain-actor/src/main/java/nl/tudelft/simulation/actor/message/policy/MessagePolicyInterface.java
+++ supplychain-actor/src/main/java/nl/tudelft/simulation/actor/message/policy/MessagePolicyInterface.java
@@ -1,22 +1,40 @@
-package nl.tudelft.simulation.actor.content;
+package nl.tudelft.simulation.actor.message.policy;
 
 import java.io.Serializable;
 
+import org.djutils.base.Identifiable;
+
+import nl.tudelft.simulation.actor.Actor;
+import nl.tudelft.simulation.actor.message.Message;
+import nl.tudelft.simulation.actor.message.MessageType;
+
 /**
- * The HandlerInterface defines what any Handler should be able to do: handle some Serializable content. <br>
+ * The HandlerInterface defines what any Message Handler should be able to do: handle the receipt of a Message. <br>
  * <br>
  * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
  * source code and binary code of this software is proprietary information of Delft University of Technology.
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
  */
-public interface HandlerInterface extends Serializable
+public interface MessagePolicyInterface extends Serializable, Identifiable
 {
     /**
      * Handle the content of the message.
-     * @param content the content to be handled, can be of any type
+     * @param message Message; the message to be handled
      * @return a boolean acknowledgement; true or false
      */
-    boolean handleContent(Serializable content);
+    boolean handleMessage(Message message);
+
+    /** 
+     * Return the message type that this handler can handle.
+     * @return MessageType; the message type that this handler can handle
+     */
+    MessageType getMessageType();
+    
+    /**
+     * Return the owner of this handler.
+     * @return owner Actor; the owner of this handler.
+     */
+    Actor getOwner();
 
 }

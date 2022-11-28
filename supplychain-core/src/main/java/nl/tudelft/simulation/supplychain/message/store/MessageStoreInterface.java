a50b00b4b7f900d76d43897b0dd873c8c3614685
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-11-28 02:08:51 +0100

    MessageStoreInterface javadoc updated

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/message/store/MessageStoreInterface.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/message/store/MessageStoreInterface.java
@@ -1,15 +1,16 @@
-package nl.tudelft.simulation.supplychain.contentstore;
+package nl.tudelft.simulation.supplychain.message.store;
 
 import java.io.Serializable;
 import java.util.List;
 
 import nl.tudelft.simulation.supplychain.actor.SupplyChainActor;
-import nl.tudelft.simulation.supplychain.content.Content;
+import nl.tudelft.simulation.supplychain.message.Message;
+import nl.tudelft.simulation.supplychain.message.MessageType;
 
 /**
- * A ContentStore is taking care of storing content for later use, for instance for matching purposes. It acts as an ERP or
- * database system for the supply chain. In this implementation, all the messages are linked to an InternalDemand, as this sets
- * off the whole chain of messages, no matter whether it is a purchase, internal production, or stock replenishment: in all
+ * A MessageStore is taking care of storing messages for later use, for instance for matching purposes. It acts as an ERP or
+ * database system for the supply chain actor. In this implementation, all the messages are linked to an InternalDemand, as this
+ * sets off the whole chain of messages, no matter whether it is a purchase, internal production, or stock replenishment: in all
  * cases the InternalDemand triggers all the other messages.
  * <p>
  * Copyright (c) 2003-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved.
@@ -18,64 +19,65 @@ import nl.tudelft.simulation.supplychain.content.Content;
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public interface ContentStoreInterface extends Serializable
+public interface MessageStoreInterface extends Serializable
 {
     /**
-     * Set the owner for the content store after is has been created. The reason for explicitly having to set the owner and not
-     * include the owner in the constructor is that the SupplyChainActor needs a ContentStore in its constructor, so the
-     * ContentStore cannot be constructed with the owner.
+     * Set the owner for the message store after is has been created. The reason for explicitly having to set the owner and not
+     * include the owner in the constructor is that the SupplyChainActor needs a MessageStore in its constructor, so the
+     * MessageStore cannot be constructed with the owner.
      * @param owner the owner
      */
     void setOwner(SupplyChainActor owner);
 
     /**
-     * Method addContent stores a new Content object into the store.
-     * @param content the content to add
+     * Method addMessage stores a new message object into the store.
+     * @param message the message to add
      * @param sent sent or not
      */
-    void addContent(Content content, boolean sent);
+    void addMessage(Message message, boolean sent);
 
     /**
-     * Method removeContent removes a Content object from the store.
-     * @param content the content to remove
-     * @param sent indicates whether the content was sent or received
+     * Method removeMessage removes a Message object from the store.
+     * @param message the message to remove
+     * @param sent indicates whether the message was sent or received
      */
-    void removeContent(Content content, boolean sent);
+    void removeMessage(Message message, boolean sent);
 
     /**
-     * Method removeSentReceivedContent removes a Content object from the sent / received store.
-     * @param content the content to remove
-     * @param sent indicates whether the content was sent or received
+     * Method removeSentReceivedMessage removes a Message object from the sent / received store.
+     * @param message the message to remove
+     * @param sent indicates whether the message was sent or received
      */
-    void removeSentReceivedContent(Content content, boolean sent);
+    void removeSentReceivedMessage(Message message, boolean sent);
 
     /**
-     * Method removeAllContent removes an exisiting Content object from the store. No error message is given when the content
+     * Method removeAllMessage removes an exisiting Message object from the store. No error message is given when the message
      * was not there; this is just ignored.
-     * @param internalDemandID the identifier of the internal demand
+     * @param internalDemandId long; the identifier of the internal demand
      */
-    void removeAllContent(Serializable internalDemandID);
+    void removeAllMessage(long internalDemandId);
 
     /**
-     * Method getContentList returns a list of Content objects of type clazz based on the internalDemandID.
-     * @param internalDemandID the identifier of the content
-     * @param clazz the content class to look for
-     * @return returns a list of content of type class based on the internalDemandID
+     * Method getMessageList returns a list of Message objects of type 'type' based on the internalDemandId.
+     * @param internalDemandId the identifier of the message
+     * @param type the message type to look for
+     * @return returns a list of message of type 'type' based on the internalDemandId
      */
-    <C extends Content> List<C> getContentList(Serializable internalDemandID, Class<C> clazz);
+    List<Message> getMessageList(long internalDemandId, MessageType type);
 
     /**
-     * Method getContentList returns the Content object of type clazz based on the internalDemandID, for either sent or received
-     * items.
-     * @param internalDemandID the identifier of the content
-     * @param clazz the content class to look for
-     * @param sent indicates whether the content was sent or received
-     * @return returns a list of content of type class based on the internalDemandID
+     * Method getMessageList returns the Message object of type 'type' based on the internalDemandId, for either sent or
+     * received items.
+     * @param internalDemandId the identifier of the message
+     * @param type the message type to look for
+     * @param sent indicates whether the message was sent or received
+     * @return returns a list of message of type 'type' based on the internalDemandId
      */
-    <C extends Content> List<C> getContentList(Serializable internalDemandID, Class<C> clazz, boolean sent);
+    List<Message> getMessageList(long internalDemandId, MessageType type, boolean sent);
 
     /**
-     * @return Returns the owner.
+     * Return the owner.
+     * @return SupplyChainActor; the owner
      */
     SupplyChainActor getOwner();
 }

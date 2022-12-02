828ae1f10af10ca9a222d129408380e80b4c0785
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-02 19:57:04 +0100

    TransportMode now has a set of handled SKUs
    So it is now possible to indicate that a Ship, Truck and Train can carry
    an ISO container, but an airplance cannot.

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/transport/TransportMode.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/transport/TransportMode.java
@@ -1,174 +1,127 @@
 package nl.tudelft.simulation.supplychain.transport;
 
 import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
-import org.djunits.unit.DurationUnit;
-import org.djunits.unit.LengthUnit;
-import org.djunits.unit.MassUnit;
-import org.djunits.unit.SpeedUnit;
-import org.djunits.value.vdouble.scalar.Duration;
-import org.djunits.value.vdouble.scalar.Length;
-import org.djunits.value.vdouble.scalar.Mass;
+import org.djunits.Throw;
 import org.djunits.value.vdouble.scalar.Speed;
-import org.djutils.draw.point.Point3d;
-import org.pmw.tinylog.Logger;
+import org.djutils.base.Identifiable;
+import org.djutils.immutablecollections.ImmutableLinkedHashSet;
+import org.djutils.immutablecollections.ImmutableSet;
 
-import nl.tudelft.simulation.dsol.animation.Locatable;
-import nl.tudelft.simulation.supplychain.actor.SupplyChainActor;
-import nl.tudelft.simulation.supplychain.finance.Money;
-import nl.tudelft.simulation.supplychain.finance.MoneyUnit;
+import nl.tudelft.simulation.supplychain.product.Sku;
 
 /**
- * <br>
+ * TransportMode stores the information on a mode of transport, to transport products from an origin to a destination.
+ * <p>
  * Copyright (c) 2003-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved.
  * <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class TransportMode implements Serializable
+public class TransportMode implements Identifiable, Serializable
 {
     /** the serial version uid. */
-    private static final long serialVersionUID = 12L;
+    private static final long serialVersionUID = 20221202L;
 
-    /** description of the transport mode. */
-    protected String name;
+    /** id (name) of the transport mode. */
+    private String id;
 
-    /** the fixed time for loading and unloading in hours. */
-    protected Duration fixedDuration;
-
-    /** average transportation speed in kilometers per hours. */
-    protected Speed speed;
-
-    /** the fixed cost for a transport. */
-    protected Money fixedCost;
-
-    /** the cost per km per kg. */
-    protected Money costKmKg;
-
-    /**
-     * predefined transport mode: plane including transport to/from the airport 250 dollar handling costs, taxes, and transport
-     * to/from airport. For the price of 0.0007 per kg per km, see conf/data/AirRates.xls
-     */
-    public static final TransportMode PLANE = new TransportMode("Plane", new Duration(8.0, DurationUnit.HOUR),
-            new Speed(800.0, SpeedUnit.KM_PER_HOUR), new Money(250.0, MoneyUnit.USD), new Money(0.0007, MoneyUnit.USD));
-
-    /**
-     * predefined transport mode: truck including transport to/from the airport 50 dollar handling costs, taxes The price is
-     * $1.50 per km, with 10000 kg on board -> 0.00015 $/km/kg.
-     */
-    public static final TransportMode TRUCK = new TransportMode("Truck", new Duration(2.0, DurationUnit.HOUR),
-            new Speed(80.0, SpeedUnit.KM_PER_HOUR), new Money(50.0, MoneyUnit.USD), new Money(0.00015, MoneyUnit.USD));
+    /** average transportation speed of the mode. */
+    private Speed averageSpeed;
+    
+    /** SKUs that the TransportMode can handle. */
+    private ImmutableSet<Sku> handledSkuSet = new ImmutableLinkedHashSet<>(new LinkedHashSet<>());
 
     /**
      * Constructor for TransportMode.
-     * @param name the name of the transport mode
-     * @param fixedTime the fixed time for loading and unloading in hrs
-     * @param speed the average transportation speed of the mode in km/hr
-     * @param fixedCost the fixed costs
-     * @param costKmKg the cost per km and kg
+     * @param id String; the name of the transport mode
+     * @param averageSpeed Speed; the average transportation speed of the mode
      */
-    public TransportMode(final String name, final Duration fixedTime, final Speed speed, final Money fixedCost,
-            final Money costKmKg)
+    public TransportMode(final String id, final Speed averageSpeed)
     {
-        super();
-        this.name = name;
-        this.fixedDuration = fixedTime;
-        this.speed = speed;
-        this.fixedCost = fixedCost;
-        this.costKmKg = costKmKg;
+        Throw.whenNull(id, "id cannot be null");
+        Throw.whenNull(averageSpeed, "averageSpeed cannot be null");
+        this.id = id;
+        this.averageSpeed = averageSpeed;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getId()
+    {
+        return this.id;
     }
 
     /**
-     * @param loc1 the origin
-     * @param loc2 the destination
-     * @return the transportation time in hours
+     * Return the average transportation speed of the mode.
+     * @return averageSpeed Speed; the average transportation speed of the mode
      */
-    public Duration transportTime(final Locatable loc1, final Locatable loc2)
+    public Speed getAverageSpeed()
     {
-        Length distance;
-        try
-        {
-            // TODO: assume km for now...
-            // TODO: change after update in dsol for Locatable
-            distance =
-                    new Length(((Point3d) loc1.getLocation()).distance(((Point3d) loc2.getLocation())), LengthUnit.KILOMETER);
-        }
-        catch (Exception exception)
-        {
-            Logger.error(exception, "transportTime");
-            return Duration.ZERO;
-        }
-        return this.transportTime(distance);
+        return this.averageSpeed;
     }
 
     /**
-     * @param actor1 the first actor
-     * @param actor2 the second actor
-     * @return the transportation time in hours
+     * Set a new the average transportation speed for the mode.
+     * @param averageSpeed Speed; a new average transportation speed for the mode
      */
-    public Duration transportTime(final SupplyChainActor actor1, final SupplyChainActor actor2)
+    public void setAverageSpeed(final Speed averageSpeed)
     {
-        Length distance = actor1.calculateDistance(actor2);
-        return this.transportTime(distance);
+        Throw.whenNull(averageSpeed, "averageSpeed cannot be null");
+        this.averageSpeed = averageSpeed;
     }
 
     /**
-     * @param distance the distance in kilometers
-     * @return the transportation time in hours
+     * Add a SKU to the set of SKUs that this TransportMode can handle.
+     * @param sku SKU; a SKU to be added to the set of SKUs that this TransportMode can handle
      */
-    public Duration transportTime(final Length distance)
+    public void addHandledSku(final Sku sku)
     {
-        return this.fixedDuration.plus(distance.divide(this.speed));
+        Throw.whenNull(sku, "sku cannot be null");
+        Set<Sku> handledSkus = this.handledSkuSet.toSet();
+        handledSkus.add(sku);
+        this.handledSkuSet = new ImmutableLinkedHashSet<>(handledSkus);
+    }
+    
+    /**
+     * Return the set of SKUs that this TransportMode can handle.
+     * @return handledSkuSet ImmutableSet&lt;Sku&gt;; the set of SKUs that this TransportMode can handle
+     */
+    public ImmutableSet<Sku> getHandledSkuSet()
+    {
+        return this.handledSkuSet;
     }
 
-    /**
-     * calculate the transport costs for weight and distance
-     * @param distance the distance in kms
-     * @param weight the weight in kgs
-     * @return the costs for transportation
-     */
-    public Money transportCosts(final Length distance, final Mass weight)
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode()
     {
-        return this.fixedCost
-                .plus(this.costKmKg.multiplyBy(distance.getInUnit(LengthUnit.KILOMETER) * weight.getInUnit(MassUnit.KILOGRAM)));
+        return Objects.hash(this.id);
     }
 
-    /**
-     * calculate the transport costs for weight and distance per unit
-     * @param distance the distance
-     * @param unitWeight the weight
-     * @return the costs for transportation per unit weight
-     */
-    public Money transportCostsPerUnit(final Length distance, final Mass unitWeight)
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:needbraces")
+    public boolean equals(final Object obj)
     {
-        return this.costKmKg.multiplyBy(distance.getInUnit(LengthUnit.KILOMETER) * unitWeight.getInUnit(MassUnit.KILOGRAM));
-    }
-
-    /**
-     * calculate the transport costs for weight and distance
-     * @param actor1 the first actor
-     * @param actor2 the second actor
-     * @param weight the weight in kgs
-     * @return the costs for transportation
-     */
-    public Money transportCosts(final SupplyChainActor actor1, final SupplyChainActor actor2, final Mass weight)
-    {
-        return this.transportCosts(actor1.calculateDistance(actor2), weight);
-    }
-
-    /**
-     * @return returns the fixed costs
-     */
-    public Money getFixedCost()
-    {
-        return this.fixedCost;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TransportMode other = (TransportMode) obj;
+        return Objects.equals(this.id, other.id);
     }
 
     /** {@inheritDoc} */
     @Override
     public String toString()
     {
-        return this.name;
+        return this.id;
     }
 }

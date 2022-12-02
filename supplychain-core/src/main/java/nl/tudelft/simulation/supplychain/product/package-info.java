e10a052c23adaf57c75ba1422af7b702e7315c84
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-02 01:03:30 +0100

    package-info for product package

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/product/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Classes to models a Product, SKU (Stock Keeping Unit), and BOM (Bill of Materials).
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+package nl.tudelft.simulation.supplychain.product;

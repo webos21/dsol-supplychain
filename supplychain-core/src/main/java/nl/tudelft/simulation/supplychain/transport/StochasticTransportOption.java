1997318d45ad876a2c5bdfce95e2cc579297a4e0
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-02 22:05:11 +0100

    TransportOptions with stochastic durations and costs

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/transport/StochasticTransportOption.java
@@ -0,0 +1,115 @@
+package nl.tudelft.simulation.supplychain.transport;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.djunits.Throw;
+import org.djutils.base.Identifiable;
+import org.djutils.immutablecollections.ImmutableArrayList;
+import org.djutils.immutablecollections.ImmutableList;
+
+/**
+ * StochasticTransportOption describes a way to get goods from A to B, with stochastically described times and costs. The class
+ * can incicate a singular transport mode that transports the goods from A to B, e.g., trucking, or a multimodal option that
+ * involves, e.g., a truck to the Port, a containrship to another port, and trucking to the final destination. Each of the modes
+ * has a different speed, and each of the transfers will take time (and possibly cost money),
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class StochasticTransportOption implements Identifiable, Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20221202L;
+
+    /** the id of the TransportOption. */
+    private final String id;
+
+    /** the sequence of TransportSteps. */
+    private ImmutableList<StochasticTransportOptionStep> transportSteps = new ImmutableArrayList<>(new ArrayList<>());
+
+    /**
+     * make a new TransportOption.
+     * @param id String; the id of the TransportOption
+     */
+    public StochasticTransportOption(final String id)
+    {
+        Throw.whenNull(id, "id cannot be null");
+        this.id = id;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getId()
+    {
+        return this.id;
+    }
+
+    /**
+     * Return the transport steps.
+     * @return ImmutableList&lt;StochasticTransportOptionStep&gt;; the transport steps
+     */
+    public ImmutableList<StochasticTransportOptionStep> getTransportSteps()
+    {
+        return this.transportSteps;
+    }
+
+    /**
+     * Add a transport step.
+     * @param transportOptionStep StochasticTransportOptionStep; the new transport step
+     */
+    public void addTransportStep(final StochasticTransportOptionStep transportOptionStep)
+    {
+        Throw.whenNull(transportOptionStep, "transportOptionStep cannot be null");
+        List<StochasticTransportOptionStep> steps = this.transportSteps.toList();
+        steps.add(transportOptionStep);
+        this.transportSteps = new ImmutableArrayList<>(steps);
+    }
+
+    /**
+     * Add a number of transport steps.
+     * @param steps List&lt;StochasticTransportOptionStep&gt;; the new transport steps
+     */
+    public void addTransportSteps(final List<StochasticTransportOptionStep> steps)
+    {
+        Throw.whenNull(steps, "steps cannot be null");
+        for (StochasticTransportOptionStep transportOptionStep : steps)
+        {
+            addTransportStep(transportOptionStep);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(this.id, this.transportSteps);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:needbraces")
+    public boolean equals(final Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StochasticTransportOption other = (StochasticTransportOption) obj;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.transportSteps, other.transportSteps);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "TransportOption [id=" + this.id + ", transportSteps=" + this.transportSteps + "]";
+    }
+
+}

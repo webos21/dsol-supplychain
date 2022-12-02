c5cabf6d1a5731dd0f16332f814355056d0af9f0
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-03 00:23:23 +0100

    Repaired the Quote in a simple way.
    Several messages to calculate delivery date, transportation time,
    transport costs, etc. can be added to make the quote more clear and
    usable.

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/message/trade/Quote.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/message/trade/Quote.java
@@ -1,12 +1,11 @@
 package nl.tudelft.simulation.supplychain.message.trade;
 
-import org.djunits.value.vdouble.scalar.Duration;
 import org.djunits.value.vdouble.scalar.Time;
 
 import nl.tudelft.simulation.supplychain.actor.SupplyChainActor;
 import nl.tudelft.simulation.supplychain.finance.Money;
 import nl.tudelft.simulation.supplychain.product.Product;
-import nl.tudelft.simulation.supplychain.transport.TransportMode;
+import nl.tudelft.simulation.supplychain.transport.TransportOption;
 
 /**
  * A Quote is an answer to a RequestForQuote (or RFQ) and indicates how many items of a certain product could be sold for a
@@ -38,11 +37,14 @@ public class Quote extends TradeMessage
     /** the date on which the goods will be sent. */
     private Time proposedShippingDate;
 
+    /** the transport option offered. */
+    private TransportOption transportOption;
+
     /** the time on the simulator clock until which the quote is valid. */
     private Time validityTime;
 
     /**
-     * The Constructor for a Quote. Note that the Quote contains a product and a n amount. This sounds superfluous, but it is
+     * The Constructor for a Quote. Note that the Quote contains a product and an amount. This sounds superfluous, but it is
      * not. The quote might contain a replacement product or less amount than the original request in the RFQ.
      * @param sender SupplyChainActor; the sender actor of the message content
      * @param receiver SupplyChainActor; the receving actor of the message content
@@ -51,13 +53,13 @@ public class Quote extends TradeMessage
      * @param amount double; the amount of products
      * @param price Money; the quotation price
      * @param proposedShippingDate Time; the intended shipping date of the products
+     * @param transportOption TransportOption; the transport option offered
      * @param validityTime Time; the time on the simulator clock until which the quote is valid
-     * @param transportMode TransportMode; the transport mode proposed
      */
     @SuppressWarnings("checkstyle:parameternumber")
     public Quote(final SupplyChainActor sender, final SupplyChainActor receiver, final RequestForQuote requestForQuote,
             final Product product, final double amount, final Money price, final Time proposedShippingDate,
-            final Time validityTime, final TransportMode transportMode)
+            final TransportOption transportOption, final Time validityTime)
     {
         super(TradeMessageTypes.QUOTE, sender, receiver, requestForQuote.getInternalDemandId());
         this.requestForQuote = requestForQuote;
@@ -65,8 +67,8 @@ public class Quote extends TradeMessage
         this.amount = amount;
         this.price = price;
         this.proposedShippingDate = proposedShippingDate;
-        this.transportMode = transportMode;
-        this.estimatedTransportationTime = this.transportMode.estimateTransportTime(sender, receiver);
+        this.transportOption = transportOption;
+        this.validityTime = validityTime;
     }
 
     /**
@@ -84,16 +86,6 @@ public class Quote extends TradeMessage
     public Product getProduct()
     {
         return this.product;
-    }
-
-    /**
-     * @return the proposedDeliveryDate.
-     */
-    public Time getProposedDeliveryDate()
-    {
-        // TODO ALEXANDER: carrier or merchant haulage?
-        return this.proposedShippingDate;
-        // return this.proposedShippingDate + this.calculatedTransportationTime;
     }
 
     /**
@@ -121,14 +113,6 @@ public class Quote extends TradeMessage
     }
 
     /**
-     * @return the calculatedTransportationTime.
-     */
-    public Duration getCalculatedTransportationTime()
-    {
-        return this.calculatedTransportationTime;
-    }
-
-    /**
      * @return the proposedShippingDate.
      */
     public Time getProposedShippingDate()
@@ -137,11 +121,11 @@ public class Quote extends TradeMessage
     }
 
     /**
-     * @return the transportMode.
+     * @return transportOption
      */
-    public TransportMode getTransportMode()
+    public TransportOption getTransportOption()
     {
-        return this.transportMode;
+        return this.transportOption;
     }
 
     /** {@inheritDoc} */

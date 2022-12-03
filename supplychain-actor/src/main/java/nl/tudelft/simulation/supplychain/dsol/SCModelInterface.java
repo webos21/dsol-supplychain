09dfa0a050e37da3c05b45ef9570dd7254341ba2
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-03 13:44:19 +0100

    SCModelInterface got a method calculateDistance

--- supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/dsol/SCModelInterface.java
+++ supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/dsol/SCModelInterface.java
@@ -1,6 +1,8 @@
 package nl.tudelft.simulation.supplychain.dsol;
 
 import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djutils.draw.point.Point;
 import org.djutils.immutablecollections.ImmutableMap;
 
 import nl.tudelft.simulation.dsol.model.DSOLModel;
@@ -53,5 +55,26 @@ public interface SCModelInterface extends DSOLModel<Duration, SCSimulatorInterfa
      * @return Map&lt;String, MessageType&gt;; the registred message types
      */
     ImmutableMap<String, MessageType> getMessageTypes();
+
+    /**
+     * Calculate the distance between two points as a Length. The point could be in lat/lon, on an orthogonal grid, or based on
+     * a GIS projection.
+     * @param loc1 Point; first location
+     * @param loc2 Point; second location
+     * @return Length; distance between the locations
+     */
+    Length calculateDistance(Point<?> loc1, Point<?> loc2);
+
+    /**
+     * Calculate the distance between two points in km. The point could be in lat/lon, on an orthogonal grid, or based on a GIS
+     * projection.
+     * @param loc1 Point; first location
+     * @param loc2 Point; second location
+     * @return double; distance between the locations in km
+     */
+    default double calculateDistanceKm(final Point<?> loc1, final Point<?> loc2)
+    {
+        return calculateDistance(loc1, loc2).si / 1000.0;
+    }
 
 }

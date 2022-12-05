0ed323759106f1305ce1838692e71c66c428e12a
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-06 00:46:55 +0100

    NoBank added for those organizations that don't need a bank
    Some constructors explicitly ask for a non-null bank instance.

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/finance/NoBank.java
@@ -0,0 +1,30 @@
+package nl.tudelft.simulation.supplychain.finance;
+
+import org.djutils.draw.point.OrientedPoint3d;
+
+import nl.tudelft.simulation.supplychain.dsol.SCSimulatorInterface;
+import nl.tudelft.simulation.supplychain.message.handler.DirectMessageHandler;
+
+/**
+ * NoBank is a class that can be used for organizations that do not need any banking services.
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class NoBank extends Bank
+{
+    /** */
+    private static final long serialVersionUID = 20221206L;
+
+    /**
+     * Create a placeholder for a Bank.
+     * @param simulator SCSimulatorInterface; needed since it cannot be null
+     */
+    public NoBank(final SCSimulatorInterface simulator)
+    {
+        super("nobank", new DirectMessageHandler(), simulator, new OrientedPoint3d(0, 0, 0), "");
+    }
+
+}

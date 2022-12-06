4e929abf2032b5abf485716a5f60b77de852dcde
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-06 10:37:02 +0100

    Interface for Actor that sells products

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/selling/SellingActorInterface.java
@@ -0,0 +1,25 @@
+package nl.tudelft.simulation.supplychain.role.selling;
+
+/**
+ * SellingActorInterface indicates that the actor has a SellingRole.
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public interface SellingActorInterface
+{
+    /**
+     * Return the selling role of this actor.
+     * @return SellingRole; the selling role of this actor
+     */
+    SellingRole getSellingRole();
+
+    /**
+     * Initialize the selling role of this actor. This method can and should only be called once.
+     * @param sellingRole SellingRole; the selling role of this actor
+     */
+    void setSellingRole(SellingRole sellingRole);
+
+}

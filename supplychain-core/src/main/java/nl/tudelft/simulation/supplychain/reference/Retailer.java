f68f292cca28d4f0a145c11d0f2e9bd536f91325
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-06 10:37:20 +0100

    Reference implementation of retailer

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/reference/Retailer.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/reference/Retailer.java
@@ -1,34 +1,33 @@
 package nl.tudelft.simulation.supplychain.reference;
 
-import java.io.Serializable;
-
+import org.djunits.Throw;
 import org.djutils.draw.point.OrientedPoint3d;
 
 import nl.tudelft.simulation.supplychain.actor.StockKeepingActor;
-import nl.tudelft.simulation.supplychain.actor.capabilities.BuyerInterface;
-import nl.tudelft.simulation.supplychain.actor.capabilities.SellerInterface;
-import nl.tudelft.simulation.supplychain.banking.Bank;
-import nl.tudelft.simulation.supplychain.contentstore.ContentStoreInterface;
 import nl.tudelft.simulation.supplychain.dsol.SCSimulatorInterface;
+import nl.tudelft.simulation.supplychain.finance.Bank;
 import nl.tudelft.simulation.supplychain.finance.Money;
-import nl.tudelft.simulation.supplychain.finance.MoneyUnit;
+import nl.tudelft.simulation.supplychain.message.Message;
+import nl.tudelft.simulation.supplychain.message.handler.MessageHandlerInterface;
+import nl.tudelft.simulation.supplychain.message.store.trade.TradeMessageStoreInterface;
 import nl.tudelft.simulation.supplychain.product.Product;
-import nl.tudelft.simulation.supplychain.roles.BuyingRole;
-import nl.tudelft.simulation.supplychain.roles.SellingRole;
+import nl.tudelft.simulation.supplychain.role.buying.BuyingActorInterface;
+import nl.tudelft.simulation.supplychain.role.buying.BuyingRole;
+import nl.tudelft.simulation.supplychain.role.selling.SellingActorInterface;
+import nl.tudelft.simulation.supplychain.role.selling.SellingRole;
 
 /**
  * Reference implementation for a Retailer.
  * <p>
- * Copyright (c) 2003-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved.
- * <br>
+ * Copyright (c) 2003-2022 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class Retailer extends StockKeepingActor implements BuyerInterface, SellerInterface
+public class Retailer extends StockKeepingActor implements BuyingActorInterface, SellingActorInterface
 {
     /** the serial version uid. */
-    private static final long serialVersionUID = 12L;
+    private static final long serialVersionUID = 20221206L;
 
     /** The role to buy. */
     private BuyingRole buyingRole = null;
@@ -37,30 +36,21 @@ public class Retailer extends StockKeepingActor implements BuyerInterface, Selle
     private SellingRole sellingRole = null;
 
     /**
-     * @param name the name of the retailer
-     * @param simulator the simulator to use
-     * @param position the position on the map
-     * @param bank the bank
-     * @param contentStore the contentStore for the messages
+     * @param name String; the name of the Customer
+     * @param messageHandler MessageHandlerInterface; the message handler to use
+     * @param simulator SCSimulatorInterface; the simulator
+     * @param location Location; the locatrion of the actor on the map or grid
+     * @param locationDescription String; a description of the location of the Customer
+     * @param bank Bank; the bank of the customer
+     * @param initialBalance Money; the initial bank balance
+     * @param messageStore TradeMessageStoreInterface; the messageStore for the messages
      */
-    public Retailer(final String name, final SCSimulatorInterface simulator, final OrientedPoint3d position, final Bank bank,
-            final ContentStoreInterface contentStore)
+    @SuppressWarnings("checkstyle:parameternumber")
+    public Retailer(final String name, final MessageHandlerInterface messageHandler, final SCSimulatorInterface simulator,
+            final OrientedPoint3d location, final String locationDescription, final Bank bank, final Money initialBalance,
+            final TradeMessageStoreInterface messageStore)
     {
-        this(name, simulator, position, bank, new Money(0.0, MoneyUnit.USD), contentStore);
-    }
-
-    /**
-     * @param name the name of the retailer
-     * @param simulator the simulator to use
-     * @param position the position on the map
-     * @param bank the bank
-     * @param initialBankAccount the initial bank balance
-     * @param contentStore the contentStore for the messages
-     */
-    public Retailer(final String name, final SCSimulatorInterface simulator, final OrientedPoint3d position, final Bank bank,
-            final Money initialBankAccount, final ContentStoreInterface contentStore)
-    {
-        super(name, simulator, position, bank, initialBankAccount, contentStore);
+        super(name, messageHandler, simulator, location, locationDescription, bank, initialBalance, messageStore);
     }
 
     /** {@inheritDoc} */
@@ -77,17 +67,13 @@ public class Retailer extends StockKeepingActor implements BuyerInterface, Selle
         return this.buyingRole;
     }
 
-    /**
-     * @param buyingRole The buyingRole to set.
-     */
+    /** {@inheritDoc} */
+    @Override
     public void setBuyingRole(final BuyingRole buyingRole)
     {
-        // remove the previous buying role
-        if (this.buyingRole != null)
-        {
-            super.removeRole(this.buyingRole);
-        }
-        super.addRole(buyingRole);
+        Throw.whenNull(buyingRole, "buyingRole cannot be null");
+        Throw.when(this.buyingRole != null, IllegalStateException.class, "buyingRole already initialized");
+        addRole(buyingRole);
         this.buyingRole = buyingRole;
     }
 
@@ -98,28 +84,22 @@ public class Retailer extends StockKeepingActor implements BuyerInterface, Selle
         return this.sellingRole;
     }
 
-    /**
-     * @param sellingRole The sellingRole to set.
-     */
+    /** {@inheritDoc} */
+    @Override
     public void setSellingRole(final SellingRole sellingRole)
     {
-        // remove the previous selling role
-        if (this.sellingRole != null)
-        {
-            super.removeRole(this.sellingRole);
-        }
-        super.addRole(sellingRole);
+        Throw.whenNull(sellingRole, "sellingRole cannot be null");
+        Throw.when(this.sellingRole != null, IllegalStateException.class, "sellingRole already initialized");
+        addRole(sellingRole);
         this.sellingRole = sellingRole;
     }
 
     /** {@inheritDoc} */
     @Override
-    public boolean handleContent(final Serializable content)
+    public void receiveMessage(final Message message)
     {
-        if (this.buyingRole == null || this.sellingRole == null)
-        {
-            throw new RuntimeException("buyingRole or sellingRole not initialized for Retailer: " + this.getName());
-        }
-        return super.handleContent(content);
+        Throw.whenNull(this.buyingRole, "BuyingRole not initialized for Customer: " + this.getName());
+        Throw.whenNull(this.sellingRole, "SellingRole not initialized for Customer: " + this.getName());
+        super.receiveMessage(message);
     }
 }

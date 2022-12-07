89e485f7123189ee0524b59635936d96653837e4
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-07 20:04:48 +0100

    SuppyChainRole added: getOwner() returns SupplyChainActor

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/actor/SupplyChainRole.java
@@ -0,0 +1,32 @@
+package nl.tudelft.simulation.supplychain.actor;
+
+/**
+ * The SupplyChainRole that is aware of the SupplyChainActor rather than the Actor.
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public abstract class SupplyChainRole extends Role
+{
+    /** */
+    private static final long serialVersionUID = 20221206L;
+
+    /**
+     * Create a SupplyChainRole that is aware of the SupplyChainActor rather than the Actor.
+     * @param owner SupplyChainActor; the owner of this role
+     */
+    public SupplyChainRole(final SupplyChainActor owner)
+    {
+        super(owner);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SupplyChainActor getOwner()
+    {
+        return (SupplyChainActor) super.getOwner();
+    }
+
+}

89bbf10efe1d3c04f581b8cc2fd669754312f842
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-09 00:44:57 +0100

    Automatic updates in risk classes

--- supplychain-risk/src/main/java/nl/tudelft/simulation/supplychain/risk/handler/AbstractRiskAwareHandler.java
+++ supplychain-risk/src/main/java/nl/tudelft/simulation/supplychain/risk/handler/AbstractRiskAwareHandler.java
@@ -3,18 +3,18 @@ package nl.tudelft.simulation.supplychain.risk.handler;
 import org.djutils.event.EventListenerInterface;
 
 import nl.tudelft.simulation.supplychain.actor.SupplyChainActor;
-import nl.tudelft.simulation.supplychain.policy.SupplyChainHandler;
+import nl.tudelft.simulation.supplychain.policy.SupplyChainPolicy;
 
 /**
  * AbstractRiskAwareHandler.java. <br>
  * <br>
- * Copyright (c) 2003-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved.
+ * Copyright (c) 2003-2022 Delft University of Technology, Delft, the Netherlands. All rights reserved.
  * <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public abstract class AbstractRiskAwareHandler extends SupplyChainHandler implements EventListenerInterface
+public abstract class AbstractRiskAwareHandler extends SupplyChainPolicy implements EventListenerInterface
 {
     /** */
     private static final long serialVersionUID = 1L;

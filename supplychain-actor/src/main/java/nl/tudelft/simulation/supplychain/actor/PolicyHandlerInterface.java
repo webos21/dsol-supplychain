04422d47c641f3c35116376b04e7a935f606df51
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-09 23:24:44 +0100

    Code formatting

--- supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/actor/PolicyHandlerInterface.java
+++ supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/actor/PolicyHandlerInterface.java
@@ -11,7 +11,8 @@ import nl.tudelft.simulation.supplychain.message.policy.MessagePolicyInterface;
  * AbstractPolicyHandler contains a set of policies for an Actor or Role, and processes Messages using the correct MessagePolicy
  * for each Message.
  * <p>
- * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2022-2022 Delft University of Technology, ProducingActorInterfaceDelft, the Netherlands. All rights reserved.
+ * <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>

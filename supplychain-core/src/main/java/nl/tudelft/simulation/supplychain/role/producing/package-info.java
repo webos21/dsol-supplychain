1e2fefb2f7ee97430c74fdc95c40bbe66cdb1d86
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-09 09:21:07 +0100

    Added 'producing' role

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/producing/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Role descriptions for Actor that produces products based on a BOM and Inventory.
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+package nl.tudelft.simulation.supplychain.role.producing;

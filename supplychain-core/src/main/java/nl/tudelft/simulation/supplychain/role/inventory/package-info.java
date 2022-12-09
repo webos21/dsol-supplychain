755225e5b996f51559b7e063fa46941f74831ab7
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-09 23:26:36 +0100

    Role for inventory management of an actor

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/inventory/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The inventory role indicates that the actor keeps stock of certain products (raw materials or products for sale).
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, ProducingActorInterfaceDelft, the Netherlands. All rights reserved.
+ * <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+package nl.tudelft.simulation.supplychain.role.inventory;

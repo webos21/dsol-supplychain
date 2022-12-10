c6a716d9885516c9da306caaa0af3d15f4a99d45
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-10 13:03:32 +0100

    Added the InventoryRole that replaces the StockKeepingActor

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/inventory/InventoryActorInterface.java
@@ -0,0 +1,27 @@
+package nl.tudelft.simulation.supplychain.role.inventory;
+
+import nl.tudelft.simulation.supplychain.actor.SupplyChainActorInterface;
+
+/**
+ * InventoryActorInterface indicates that the actor has a InventoryRole.
+ * <p>
+ * Copyright (c) 2022-2022 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public interface InventoryActorInterface extends SupplyChainActorInterface
+{
+    /**
+     * Return the inventory role of this actor.
+     * @return InventoryRole; the inventory role of this actor
+     */
+    InventoryRole getInventoryRole();
+
+    /**
+     * Initialize the inventory role of this actor. This method can and should only be called once.
+     * @param inventoryRole InventoryRole; the inventory role of this actor
+     */
+    void setInventoryRole(InventoryRole inventoryRole);
+
+}
---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/inventory/InventoryRole.java
@@ -0,0 +1,70 @@
+package nl.tudelft.simulation.supplychain.role.inventory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.djunits.Throw;
+
+import nl.tudelft.simulation.supplychain.actor.SupplyChainRole;
+import nl.tudelft.simulation.supplychain.inventory.Inventory;
+import nl.tudelft.simulation.supplychain.inventory.InventoryInterface;
+import nl.tudelft.simulation.supplychain.product.Product;
+
+/**
+ * The inventory role is a role that handles the storage of products, which can be raw materials for production or finished
+ * goods. The InventoyRole can trigger production and purchasing to replenish the inventory.
+ * <p>
+ * Copyright (c) 2003-2022 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public abstract class InventoryRole extends SupplyChainRole
+{
+    /** */
+    private static final long serialVersionUID = 20221206L;
+
+    /** the inventory with products. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected final InventoryInterface inventory;
+
+    /**
+     * Create a ProducingRole object for an actor.
+     * @param owner SupplyChainActorInterface; the owner of this role
+     * @param initialInventory InventoryInterface; the Inventory to use within this role
+     */
+    public InventoryRole(final InventoryActorInterface owner, final InventoryInterface initialInventory)
+    {
+        super(owner);
+        Throw.whenNull(initialInventory, "initialInventory cannot be null");
+        this.inventory = new Inventory(owner, initialInventory);
+    }
+
+    /**
+     * Implement to check whether the stock is below some level, might trigger ordering of extra amount of the product.
+     * @param product Product; the product to check the stock for.
+     */
+    public abstract void checkStock(Product product);
+
+    /**
+     * @return the raw materials
+     */
+    public List<Product> getProductsOnStock()
+    {
+        List<Product> products = new ArrayList<Product>();
+        for (Product product : this.inventory.getProducts())
+        {
+            products.add(product);
+        }
+        return products;
+    }
+
+    /**
+     * Return the stock of this Actor.
+     * @return StockInterface; the stock of this Actor
+     */
+    public InventoryInterface getStock()
+    {
+        return this.inventory;
+    }
+}

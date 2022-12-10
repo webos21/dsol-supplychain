94c3f0052cf0e7811b32ee15cab46002bdbeffc1
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-10 13:04:07 +0100

    Updated interfaces for reference classes

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/reference/Manufacturer.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/reference/Manufacturer.java
@@ -1,7 +1,6 @@
 package nl.tudelft.simulation.supplychain.reference;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.djutils.draw.point.OrientedPoint3d;

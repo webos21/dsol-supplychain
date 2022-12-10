7834706a8b6636e164a7911a8c0b8af4522235d5
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-10 23:02:56 +0100

    Remove Production class and move contents to ProducingRole

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/producing/ProducingRole.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/producing/ProducingRole.java
@@ -1,6 +1,13 @@
 package nl.tudelft.simulation.supplychain.role.producing;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.pmw.tinylog.Logger;
+
 import nl.tudelft.simulation.supplychain.actor.SupplyChainRole;
+import nl.tudelft.simulation.supplychain.message.trade.ProductionOrder;
+import nl.tudelft.simulation.supplychain.product.Product;
 import nl.tudelft.simulation.supplychain.role.inventory.InventoryActorInterface;
 
 /**
@@ -18,6 +25,9 @@ public abstract class ProducingRole extends SupplyChainRole
     /** */
     private static final long serialVersionUID = 20221206L;
 
+    /** the production services per product for this role. */
+    private Map<Product, ProductionServiceInterface> productionServices = new LinkedHashMap<>();
+
     /**
      * Create a ProducingRole object for an actor.
      * @param owner SupplyChainActorInterface; the owner of this role
@@ -27,4 +37,40 @@ public abstract class ProducingRole extends SupplyChainRole
         super(owner);
     }
 
+    /**
+     * Add a production service.
+     * @param productionService the service to add
+     */
+    public void addProductionService(final ProductionService productionService)
+    {
+        this.productionServices.put(productionService.getProduct(), productionService);
+    }
+
+    /**
+     * Method acceptProductionOrder gets the correct production service and starts producing.
+     * @param productionOrder the production order.
+     * @return returns false if no production service could be found
+     */
+    public boolean acceptProductionOrder(final ProductionOrder productionOrder)
+    {
+        if (this.productionServices.containsKey(productionOrder.getProduct()))
+        {
+            Logger.trace("Production for actor '{}': acceptProductionOrder: production service found for product: {}",
+                    getOwner().getName(), productionOrder.getProduct().getName());
+            this.productionServices.get(productionOrder.getProduct()).acceptProductionOrder(productionOrder);
+            return true;
+        }
+        Logger.trace("Production for actor '{}': acceptProductionOrder: could not find production service for product: {}",
+                getOwner().getName(), productionOrder.getProduct().getName());
+        return false;
+    }
+
+    /**
+     * Method getProductionServices.
+     * @return returns the production services
+     */
+    public Map<Product, ProductionServiceInterface> getProductionServices()
+    {
+        return this.productionServices;
+    }
 }

60b52026e93f12217b37a3fe1402a3fe3ed4034a
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-12-10 12:45:04 +0100

    ActorInterface is used rather than Actor

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/actor/SupplyChainRole.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/actor/SupplyChainRole.java
@@ -3,8 +3,7 @@ package nl.tudelft.simulation.supplychain.actor;
 /**
  * The SupplyChainRole that is aware of the SupplyChainActor rather than the Actor.
  * <p>
- * Copyright (c) 2022-2022 Delft University of Technology, Delft, the Netherlands. All rights reserved.
- * <br>
+ * Copyright (c) 2022-2022 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
@@ -16,7 +15,7 @@ public abstract class SupplyChainRole extends Role
 
     /**
      * Create a SupplyChainRole that is aware of the SupplyChainActor rather than the Actor.
-     * @param owner SupplyChainActor; the owner of this role
+     * @param owner SupplyChainActorInterface; the owner of this role
      */
     public SupplyChainRole(final SupplyChainActorInterface owner)
     {

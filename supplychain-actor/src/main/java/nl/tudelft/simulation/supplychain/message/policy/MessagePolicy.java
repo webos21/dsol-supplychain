7de95f8e11811d48800b6f2078ffdac5060ead26
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-08 15:50:37 +0200

    Rename AbstractMessagePolicy -> MessagePolicy

--- supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/message/policy/MessagePolicy.java
+++ supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/message/policy/MessagePolicy.java
@@ -1,12 +1,15 @@
 package nl.tudelft.simulation.supplychain.message.policy;
 
+import java.io.Serializable;
+
+import org.djutils.base.Identifiable;
 import org.djutils.exceptions.Throw;
 
-import nl.tudelft.simulation.supplychain.actor.Actor;
+import nl.tudelft.simulation.supplychain.actor.Role;
 import nl.tudelft.simulation.supplychain.message.Message;
 
 /**
- * An abstract definition of a message policy with an owner.
+ * An abstract definition of a message policy with a role as the owner.
  * <p>
  * Copyright (c) 2003-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
@@ -14,7 +17,7 @@ import nl.tudelft.simulation.supplychain.message.Message;
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @param <M> the message class for which this policy applies
  */
-public abstract class AbstractMessagePolicy<M extends Message> implements MessagePolicyInterface<M>
+public abstract class MessagePolicy<M extends Message> implements Identifiable, Serializable
 {
     /** the serial version uid. */
     private static final long serialVersionUID = 20221126L;
@@ -22,8 +25,8 @@ public abstract class AbstractMessagePolicy<M extends Message> implements Messag
     /** the id of the policy. */
     private final String id;
 
-    /** the owner of this policy. */
-    private final Actor owner;
+    /** the role that owns this policy. */
+    private final Role role;
 
     /** the class of messages for which this policy applies. */
     private final Class<M> messageClass;
@@ -31,18 +34,25 @@ public abstract class AbstractMessagePolicy<M extends Message> implements Messag
     /**
      * constructs a new message policy.
      * @param id String; the id of the policy
-     * @param owner Actor; the owner of this policy
+     * @param role Role; the role that owns this policy
      * @param messageClass Class&lt;M&gt;; the message type that this policy can process
      */
-    public AbstractMessagePolicy(final String id, final Actor owner, final Class<M> messageClass)
+    public MessagePolicy(final String id, final Role role, final Class<M> messageClass)
     {
         Throw.whenNull(id, "id cannot be null");
-        Throw.whenNull(owner, "owner cannot be null");
+        Throw.whenNull(role, "role cannot be null");
         Throw.whenNull(messageClass, "messageClass cannot be null");
         this.id = id;
-        this.owner = owner;
+        this.role = role;
         this.messageClass = messageClass;
     }
+
+    /**
+     * Handle the content of the message.
+     * @param message M; the message to be handled
+     * @return a boolean acknowledgement; true or false
+     */
+    public abstract boolean handleMessage(M message);
 
     /** {@inheritDoc} */
     @Override
@@ -51,18 +61,23 @@ public abstract class AbstractMessagePolicy<M extends Message> implements Messag
         return this.id;
     }
 
-    /** {@inheritDoc} */
-    @Override
-    public Actor getOwner()
+    /**
+     * Return the role to which this handler belongs.
+     * @return role Role; the role to which this handler belongs
+     */
+    public Role getRole()
     {
-        return this.owner;
+        return this.role;
     }
 
-    /** {@inheritDoc} */
-    @Override
+    /**
+     * Return the class of messages for which this policy applies.
+     * @return Class&lt;? extends M&gt;; the class of messages for which this policy applies
+     */
     public Class<M> getMessageClass()
     {
         return this.messageClass;
     }
-
+    
+    
 }

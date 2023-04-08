51acd6f6843622d17bed5531d1e633e9dc9a727e
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-08 15:50:05 +0200

    Fold MessagePolicyInterface and AbstractMessagePolicy

--- supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/message/policy/MessagePolicy.java
+++ supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/message/policy/MessagePolicy.java
@@ -1,14 +1,12 @@
 package nl.tudelft.simulation.supplychain.message.policy;
 
-import java.io.Serializable;
-
-import org.djutils.base.Identifiable;
+import org.djutils.exceptions.Throw;
 
 import nl.tudelft.simulation.supplychain.actor.Actor;
 import nl.tudelft.simulation.supplychain.message.Message;
 
 /**
- * The HandlerInterface defines what any Message Handler should be able to do: handle the receipt of a Message.
+ * An abstract definition of a message policy with an owner.
  * <p>
  * Copyright (c) 2003-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
@@ -16,24 +14,55 @@ import nl.tudelft.simulation.supplychain.message.Message;
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @param <M> the message class for which this policy applies
  */
-public interface MessagePolicyInterface<M extends Message> extends Serializable, Identifiable
+public abstract class AbstractMessagePolicy<M extends Message> implements MessagePolicyInterface<M>
 {
-    /**
-     * Handle the content of the message.
-     * @param message M; the message to be handled
-     * @return a boolean acknowledgement; true or false
-     */
-    boolean handleMessage(M message);
+    /** the serial version uid. */
+    private static final long serialVersionUID = 20221126L;
+
+    /** the id of the policy. */
+    private final String id;
+
+    /** the owner of this policy. */
+    private final Actor owner;
+
+    /** the class of messages for which this policy applies. */
+    private final Class<M> messageClass;
 
     /**
-     * Return the owner of this handler.
-     * @return owner Actor; the owner of this handler.
+     * constructs a new message policy.
+     * @param id String; the id of the policy
+     * @param owner Actor; the owner of this policy
+     * @param messageClass Class&lt;M&gt;; the message type that this policy can process
      */
-    Actor getOwner();
+    public AbstractMessagePolicy(final String id, final Actor owner, final Class<M> messageClass)
+    {
+        Throw.whenNull(id, "id cannot be null");
+        Throw.whenNull(owner, "owner cannot be null");
+        Throw.whenNull(messageClass, "messageClass cannot be null");
+        this.id = id;
+        this.owner = owner;
+        this.messageClass = messageClass;
+    }
 
-    /**
-     * Return the class of messages for which this policy applies.
-     * @return Class&lt;? extends M&gt;; the class of messages for which this policy applies
-     */
-    Class<M> getMessageClass();
+    /** {@inheritDoc} */
+    @Override
+    public String getId()
+    {
+        return this.id;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Actor getOwner()
+    {
+        return this.owner;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Class<M> getMessageClass()
+    {
+        return this.messageClass;
+    }
+
 }

2e387b0746a27b18e7b16b11285b9516ecd46582
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-08 17:22:41 +0200

    New implementation of Role with MessageReceiver

--- supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/actor/Role.java
+++ supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/actor/Role.java
@@ -1,42 +1,171 @@
 package nl.tudelft.simulation.supplychain.actor;
 
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import org.djutils.base.Identifiable;
+import org.djutils.event.EventListenerMap;
+import org.djutils.event.EventProducer;
+import org.djutils.event.LocalEventProducer;
 import org.djutils.exceptions.Throw;
+
+import nl.tudelft.simulation.supplychain.message.Message;
+import nl.tudelft.simulation.supplychain.message.policy.MessagePolicy;
+import nl.tudelft.simulation.supplychain.message.receiver.MessageReceiver;
 
 /**
  * Role is a template for a consistent set of policies for handling messages, representing a certain part of the organization,
- * such as sales, inventory, finance, or purchasing.
+ * such as sales, inventory, finance, or purchasing. When a Role receives a message, it goes first through the MessageReceiver
+ * that can cause delay (e.g., schedules, use of resources). After that, the Role dispatches the messages to one or more
+ * MessagePolicy classes that handle the messages.
  * <p>
  * Copyright (c) 2022-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public abstract class Role extends AbstractPolicyHandler
+public abstract class Role implements EventProducer, Identifiable, Serializable
 {
     /** */
     private static final long serialVersionUID = 20221121L;
 
+    /** the id of the role. */
+    private final String id;
+
     /** the actor to which this role belongs. */
-    private final Actor owner;
+    private final Actor actor;
+
+    /** the message handler. */
+    private final MessageReceiver messageReceiver;
+
+    /** the message handling policies. */
+    private final Map<Class<? extends Message>, MessagePolicy<? extends Message>> messagePolicies = new LinkedHashMap<>();
+
+    /** the embedded event producer. */
+    private final EventProducer eventProducer;
 
     /**
      * Create a new Role.
-     * @param owner Actor; the actor to which this role belongs
+     * @param id String; the id of the role
+     * @param actor Actor; the actor to which this role belongs
+     * @param messageReceiver MessageReceiver; the message handler to use for processing the messages
+     * @param eventProducer EventProducer; a special EventProducer to use, e.g., a RmiEventProducer
      */
-    public Role(final Actor owner)
+    public Role(final String id, final Actor actor, final MessageReceiver messageReceiver, final EventProducer eventProducer)
     {
-        super(owner.getSimulator());
-        Throw.whenNull(owner, "owner cannot be null");
-        this.owner = owner;
+        Throw.whenNull(id, "id cannot be null");
+        Throw.whenNull(actor, "actor cannot be null");
+        Throw.whenNull(messageReceiver, "messageReceiver cannot be null");
+        Throw.whenNull(eventProducer, "eventProducer cannot be null");
+        this.id = id;
+        this.actor = actor;
+        this.messageReceiver = messageReceiver;
+        this.eventProducer = eventProducer;
+        this.messageReceiver.setRole(this);
+    }
+
+    /**
+     * Create a new Role.
+     * @param id String; the id of the role
+     * @param actor Actor; the actor to which this role belongs
+     * @param messageReceiver MessageReceiver; the message handler to use for processing the messages
+     */
+    public Role(final String id, final Actor actor, final MessageReceiver messageReceiver)
+    {
+        this(id, actor, messageReceiver, new LocalEventProducer());
+    }
+
+    /**
+     * Set a message handling policy for a message type, possibly overwriting the previous message handling policy.
+     * @param policy MessagePolicyInterface&lt;M&gt;; the policy to add
+     * @param <M> the message type
+     */
+    public <M extends Message> void setMessagePolicy(final MessagePolicy<M> policy)
+    {
+        Throw.whenNull(policy, "policy cannot be null");
+        this.messagePolicies.put(policy.getMessageClass(), policy);
+    }
+
+    /**
+     * Remove a message handling policy for a mesage type.
+     * @param messageClass Class&lt;M&gt;; the message class of the policy to remove
+     * @param <M> the message type
+     */
+    public <M extends Message> void removeMessagePolicy(final Class<M> messageClass)
+    {
+        Throw.whenNull(messageClass, "messageClass cannot be null");
+        this.messagePolicies.remove(messageClass);
+    }
+
+    /**
+     * This is the core processing of a message that was received. All appropriate policies of the actor or role are executed.
+     * @param message M; the message to process
+     * @param <M> The message class to ensure that the message and policy align
+     * @return boolean; whether the PolicyHandler processed the message or not
+     */
+    @SuppressWarnings("unchecked")
+    public <M extends Message> boolean handleMessage(final M message)
+    {
+        if (!this.messagePolicies.containsKey(message.getClass()))
+        {
+            return false;
+        }
+        this.messageReceiver.receiveMessage(message, (MessagePolicy<M>) this.messagePolicies.get(message.getClass()));
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public EventListenerMap getEventListenerMap() throws RemoteException
+    {
+        return this.eventProducer.getEventListenerMap();
     }
 
     /**
      * Return the actor to which this role belongs.
      * @return owner Actor; the actor to which this role belongs
      */
-    public Actor getOwner()
+    public Actor getActor()
     {
-        return this.owner;
+        return this.actor;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getId()
+    {
+        return this.id;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(this.actor, this.id);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        return Objects.equals(this.actor, other.actor) && Objects.equals(this.id, other.id);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return this.actor.getId() + "." + this.id;
     }
 
 }

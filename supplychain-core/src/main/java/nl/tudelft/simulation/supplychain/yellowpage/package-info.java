6bb42ddf0f76a74174909bd397beffb44905ae5e
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-09 22:13:36 +0200

    yellowpage package replaced by role.yellowpage

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/yellowpage/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Yellow Page classes to look up Actors with certain properties.
- * <p>
- * Copyright (c) 2022-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
- * The supply chain Java library uses a BSD-3 style license.
- * </p>
- * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
- */
-package nl.tudelft.simulation.supplychain.yellowpage;

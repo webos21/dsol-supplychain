31a331afaec5042846e9cbecff55ff6547cb2080
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-12 03:47:10 +0200

    Remove the supplychain-actor project
    It has been merged into supplychain-core

--- supplychain-actor/src/main/java/nl/tudelft/simulation/supplychain/json/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * JSON parser for messages, using adapters for special types.
- * <p>
- * Copyright (c) 2023-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
- * The supply chain Java library uses a BSD-3 style license.
- * </p>
- * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
- */
-package nl.tudelft.simulation.supplychain.json;

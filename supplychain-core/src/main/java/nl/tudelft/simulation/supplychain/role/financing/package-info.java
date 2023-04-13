b779f2627aa1eba414f7495df8969c65dfb34c57
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-13 20:07:07 +0200

    Add financing package

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/financing/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The financing role of an organization takes care of billing and handling payments.
+ * <p>
+ * Copyright (c) 2023-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+package nl.tudelft.simulation.supplychain.role.financing;

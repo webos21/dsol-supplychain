ff73a90b40fef65508df20946b5a0da44773bd41
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-15 15:28:53 +0200

    Updated TestActor for unit tests

--- supplychain-core/src/test/java/nl/tudelft/supplychain/actor/TestActor.java
+++ supplychain-core/src/test/java/nl/tudelft/supplychain/actor/TestActor.java
@@ -2,8 +2,8 @@ package nl.tudelft.supplychain.actor;
 
 import org.djutils.draw.point.OrientedPoint2d;
 
-import nl.tudelft.simulation.supplychain.actor.Actor;
 import nl.tudelft.simulation.supplychain.actor.ActorAlreadyDefinedException;
+import nl.tudelft.simulation.supplychain.actor.SupplyChainActor;
 import nl.tudelft.simulation.supplychain.dsol.SupplyChainModelInterface;
 import nl.tudelft.simulation.supplychain.message.store.trade.TradeMessageStore;
 
@@ -15,13 +15,20 @@ import nl.tudelft.simulation.supplychain.message.store.trade.TradeMessageStore;
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class TestActor extends Actor
+public class TestActor extends SupplyChainActor
 {
     private static final long serialVersionUID = 1L;
 
-    public TestActor(final String id, final String name, final SupplyChainModelInterface model,
-            final OrientedPoint2d location, final String locationDescription) throws ActorAlreadyDefinedException
+    public TestActor(final String id, final String name, final SupplyChainModelInterface model, final OrientedPoint2d location,
+            final String locationDescription) throws ActorAlreadyDefinedException
     {
-        super(id, name, model, location, locationDescription, null, null, new TradeMessageStore());
+        super(id, name, model, location, locationDescription, new TradeMessageStore());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void checkNecessaryRoles()
+    {
+        // nothing to check, no roles
     }
 }

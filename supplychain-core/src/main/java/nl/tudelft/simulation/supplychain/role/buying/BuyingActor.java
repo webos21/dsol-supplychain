844c096d3f37258363bb5a086f8abec643ba9b06
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-15 15:15:44 +0200

    Javadoc

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/buying/BuyingActor.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/buying/BuyingActor.java
@@ -3,7 +3,7 @@ package nl.tudelft.simulation.supplychain.role.buying;
 import nl.tudelft.simulation.supplychain.actor.Actor;
 
 /**
- * Buying is an interface to indicate that an Actor has a BuyingRole.
+ * BuyingActor is an interface to indicate that an Actor has a BuyingRole.
  * <p>
  * Copyright (c) 2023-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.

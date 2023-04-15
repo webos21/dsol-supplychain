21f0f37a682dfdfb35624fd9527b091187b710bf
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-15 22:51:30 +0200

    BuyingActor and SelligActor extend FinancingActor
    Typically, buying and selling involve bills and payments.

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/buying/BuyingActor.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/buying/BuyingActor.java
@@ -1,16 +1,17 @@
 package nl.tudelft.simulation.supplychain.role.buying;
 
-import nl.tudelft.simulation.supplychain.actor.Actor;
+import nl.tudelft.simulation.supplychain.role.financing.FinancingActor;
 
 /**
- * BuyingActor is an interface to indicate that an Actor has a BuyingRole.
+ * BuyingActor is an interface to indicate that an Actor has a BuyingRole. Since Buying usually involves bills and payments, the
+ * BuyingActor also implements the FinancingActor.
  * <p>
  * Copyright (c) 2023-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public interface BuyingActor extends Actor
+public interface BuyingActor extends FinancingActor
 {
     /**
      * Return the BuyingRole for this actor.
--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/selling/SellingActor.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/selling/SellingActor.java
@@ -1,16 +1,17 @@
 package nl.tudelft.simulation.supplychain.role.selling;
 
-import nl.tudelft.simulation.supplychain.actor.Actor;
+import nl.tudelft.simulation.supplychain.role.financing.FinancingActor;
 
 /**
- * SellingActor is an interface to indicate that an Actor has a SellingRole.
+ * SellingActor is an interface to indicate that an Actor has a SellingRole. Since Selling usually involves bills and payments,
+ * the SellingActor also implements the FinancingActor.
  * <p>
  * Copyright (c) 2023-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public interface SellingActor extends Actor
+public interface SellingActor extends FinancingActor
 {
     /**
      * Return the SellingRole for this actor.
